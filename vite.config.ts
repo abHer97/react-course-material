7c851650a09e9cd8b47ad2bac4d02cac6be23f3c
Abel Herrera <ahpard97@gmail.com>
2024-02-19 20:56:13 -0600

    chore: configure tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // Habilita vitest en todos los archivos de test sin necesidad de importar las funciones de test
+    globals: true,
+    // Le dice a vitest que archivos seran los que se usaran para tests
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    // Para que @testing-library pueda montar los componentes de react en un dom (sin navegador) y testear
+    environment: 'jsdom',
+  },
+});
