d9a05af48749189623fafca0bae273c6f3a5f1df
Abel Herrera <ahpard97@gmail.com>
2024-02-19 21:45:56 -0600

    feat: create useToasts hook

---
+++ src/4-notificaciones-toast/toasts/hooks/use-toasts/use-toasts.tsx
@@ -0,0 +1,16 @@
+import { useMemo } from 'react';
+
+import { toastsStore } from '../../state/toast-store';
+import { ToastFactory } from '../../domain/factories/toast-factory';
+
+export function useToasts() {
+  const methods = useMemo(() => {
+    return {
+      info(message: string) {
+        toastsStore.setState(ToastFactory.info({ message }));
+      },
+    };
+  }, []);
+
+  return methods;
+}
