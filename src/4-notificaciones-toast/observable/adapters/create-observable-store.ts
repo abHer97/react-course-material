65a3f82cf5dcd3b04bddebc9dec43dbdcf81302f
Abel Herrera <ahpard97@gmail.com>
2024-02-19 21:25:36 -0600

    upd: enable notify subscribers when setState changes the current state

--- src/4-notificaciones-toast/observable/adapters/create-observable-store.ts
+++ src/4-notificaciones-toast/observable/adapters/create-observable-store.ts
@@ -1,9 +1,10 @@
 import { IObservableStore } from '../entities/observable-store';
+import { Subscriber } from '../entities/subscriber';
 
 export function createObservableStore<TState extends object>(
   initialState: TState
 ): IObservableStore<TState> {
-  const subscribers = new Set(); // Set es como un arreglo pero no permite agregar elementos duplicados
+  const subscribers = new Set<Subscriber<TState>>(); // Set es como un arreglo pero no permite agregar elementos duplicados
   let state = initialState;
 
   return {
@@ -12,6 +13,8 @@ export function createObservableStore<TState extends object>(
     },
     setState(newState) {
       state = newState;
+
+      subscribers.forEach((sub) => sub(state));
     },
     subscribe(subscriber) {
       subscribers.add(subscriber);
