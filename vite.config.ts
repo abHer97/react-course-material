4a1181456ecfcbabac90186f7e5c1b0d2070e815
Abel Herrera <ahpard97@gmail.com>
2024-02-24 22:51:01 -0600

    chore: configure github deployments

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   test: {
     // Habilita vitest en todos los archivos de test sin necesidad de importar las funciones de test
