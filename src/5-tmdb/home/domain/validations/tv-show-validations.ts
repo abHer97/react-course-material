880926406e8479bf348bcffceef2c998d063a286
Abel Herrera <ahpard97@gmail.com>
2024-02-25 19:47:34 -0600

    feat: create tvShow validations

---
+++ src/5-tmdb/home/domain/validations/tv-show-validations.ts
@@ -0,0 +1,32 @@
+import { array, boolean, enum_, number, object, parse, string } from 'valibot';
+
+import { ITvShow } from '../entities/tv-show';
+import { MediaType } from '../entities/media-type';
+
+const tvShowValidationSchema = object({
+  adult: boolean(),
+  backdrop_path: string(),
+  id: number(),
+  name: string(),
+  original_language: string(),
+  original_name: string(),
+  overview: string(),
+  poster_path: string(),
+  media_type: enum_(MediaType),
+  genre_ids: array(number()),
+  popularity: number(),
+  first_air_date: string(),
+  vote_average: number(),
+  vote_count: number(),
+  origin_country: array(string()),
+});
+
+export function isTvShow(data: unknown): data is ITvShow {
+  try {
+    parse(tvShowValidationSchema, data);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
