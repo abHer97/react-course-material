a97ab1d4f8960c2ae50c71ffbc29f63ff9e3c0ef
Abel Herrera <ahpard97@gmail.com>
2024-03-02 15:12:29 -0600

    upd: add movie entity

--- src/5-tmdb/movies/domain/entities/movie.ts
+++ src/5-tmdb/movies/domain/entities/movie.ts
@@ -9,11 +9,63 @@ export interface IPartialMovie {
   original_title: string;
   overview: string;
   poster_path: string;
-  media_type: MediaType;
-  genre_ids: number[];
+  media_type: MediaType; //
+  genre_ids: number[]; //
   popularity: number;
   release_date: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface IMovie extends Omit<IPartialMovie, 'media_type' | 'genre_ids'> {
+  // adult: boolean;
+  // backdrop_path: string;
+  // TODO: Verify the type of below field
+  // belongs_to_collection?: any;
+  // id: number;
+  // original_language: string;
+  // original_title: string;
+  // overview: string;
+  // popularity: number;
+  // poster_path: string;
+  // release_date: string;
+  // title: string;
+  // video: boolean;
+  // vote_average: number;
+  // vote_count: number;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  imdb_id: string;
+  production_companies: Productioncompany[];
+  production_countries: Productioncountry[];
+  revenue: number;
+  runtime: number;
+  spoken_languages: Spokenlanguage[];
+  status: string;
+  tagline: string;
+}
+
+interface Spokenlanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface Productioncountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface Productioncompany {
+  id: number;
+  logo_path?: string;
+  name: string;
+  origin_country: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
 }
