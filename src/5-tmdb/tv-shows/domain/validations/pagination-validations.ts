b3fa0a58974bdb78f99f6c640514271d537d59a3
Abel Herrera <ahpard97@gmail.com>
2024-03-02 11:33:11 -0600

    feat: create tvShow entities

--- src/5-tmdb/tv-shows/domain/validations/pagination-validations.ts
+++ src/5-tmdb/tv-shows/domain/validations/pagination-validations.ts
@@ -1,12 +1,37 @@
-import { number, object, parse } from 'valibot';
+import {
+  ArraySchema,
+  BaseSchema,
+  NumberSchema,
+  ObjectEntries,
+  ObjectSchema,
+  StringSchema,
+  number,
+  object,
+  parse,
+} from 'valibot';
 
 import { IPagination } from '../entities/pagination';
 
-const paginationSchema = object({
+export const paginationSchema = object({
   page: number(),
   total_pages: number(),
   total_results: number(),
 });
+
+export function createPaginationSchema<T>(
+  schema: T extends ObjectEntries
+    ? ObjectSchema<T>
+    : T extends BaseSchema
+    ? ArraySchema<T>
+    : NumberSchema | StringSchema
+) {
+  return object({
+    page: number(),
+    total_pages: number(),
+    total_results: number(),
+    results: schema,
+  });
+}
 
 export function isPagination(data: unknown): data is IPagination {
   try {
@@ -16,4 +41,8 @@ export function isPagination(data: unknown): data is IPagination {
   } catch {
     return false;
   }
+}
+
+export function validatePagination(data: unknown): asserts data is IPagination {
+  parse(paginationSchema, data);
 }
