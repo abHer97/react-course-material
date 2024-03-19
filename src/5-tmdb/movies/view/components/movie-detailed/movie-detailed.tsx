81f464c3289008b617722e29406462488595ce41
Abel Herrera <ahpard97@gmail.com>
2024-03-18 23:02:55 -0600

    upd: add responsive css

--- src/5-tmdb/movies/view/components/movie-detailed/movie-detailed.tsx
+++ src/5-tmdb/movies/view/components/movie-detailed/movie-detailed.tsx
@@ -6,25 +6,29 @@ import { MoviePercentage } from '../movie-percentage/movie-percentage';
 export function MovieDetailed({ movie }: { movie: IMovie }) {
   return (
     <section className='flex flex-col gap-4'>
-      <div className='flex flex-row gap-4'>
+      <div className='flex flex-col md:flex-row gap-4'>
         <Img fileSize={FileSize.w200} src={movie.poster_path} />
-        <article>
-          <h2>{movie.title}</h2>
-          <div className='flex flex-row gap-2 text-sm items-center'>
-            <p>{movie.release_date}</p>
-            <ul className='flex flex-row gap-2 text-gray-600'>
-              {movie.genres.map((genre) => {
-                return <li key={genre.id}>{genre.name}</li>;
-              })}
-            </ul>
+        <article className='flex flex-col gap-4'>
+          <div>
+            <h2>{movie.title}</h2>
+            <div className='flex flex-row gap-2 text-sm items-center w-full overflow-x-scroll'>
+              <p className='shrink-0'>{movie.release_date}</p>
+              <ul className='flex flex-row gap-2 text-gray-600'>
+                {movie.genres.map((genre) => {
+                  return <li key={genre.id}>{genre.name}</li>;
+                })}
+              </ul>
+            </div>
           </div>
           <div className='flex flex-row items-center gap-2'>
             <MoviePercentage movieVoteAverage={movie.vote_average} />
             <span>Puntuacion de usuario</span>
           </div>
-          <p className='text-sm text-gray-600 italic'>{movie.tagline}</p>
-          <span>Resumen</span>
-          <p className='font-light text-sm'>{movie.overview}</p>
+          <div>
+            <p className='text-sm text-gray-600 italic'>{movie.tagline}</p>
+            <p>Resumen</p>
+            <p className='font-light text-sm'>{movie.overview}</p>
+          </div>
         </article>
       </div>
       <MovieCredits movieId={movie.id} />
