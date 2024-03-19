7596a810dfef6f8e3139ad44e72435b0d99b6b24
Abel Herrera <ahpard97@gmail.com>
2024-03-18 22:18:07 -0600

    upd: add accountId v field

--- src/5-tmdb/config/env-vars.ts
+++ src/5-tmdb/config/env-vars.ts
@@ -2,4 +2,5 @@ import { isString } from '../shared/domain/validations/string';
 
 export const ENV_VARS = {
   baseUrl: isString(import.meta.env.VITE_BASE_URL) ? import.meta.env.VITE_BASE_URL : '',
+  accountId: Number(import.meta.env.VITE_ACCOUNT_ID || 0),
 };
